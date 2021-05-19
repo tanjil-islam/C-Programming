97128bf2e211b6d2f2af0f4a29da77fcb806cfe8
tanjil-islam <tanjil.islam040820@gmail.com>
2021-05-19 23:32:00 +0600

    Simple array

---
+++ array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[5];
+
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    arr[4]=5;
+    int i=0;
+    for(i=0;i<5;i++){
+        printf("%d",arr[i]);
+        printf("\n");
+    }
+
+    return 0;
+}

59dc91823a546dd45ea56c93d3f15543b3d5b21f
tanjil-islam <tanjil.islam040820@gmail.com>
2021-05-19 23:36:37 +0600

    Modify array

--- array.c
+++ array.c
@@ -3,13 +3,14 @@
 int main()
 {
     int arr[5];
-
-    arr[0]=1;
-    arr[1]=2;
-    arr[2]=3;
-    arr[3]=4;
-    arr[4]=5;
     int i=0;
+
+    for(i=0;i<5;i++){
+       scanf("%d",&arr[i]);
+    }
+
+    printf("\nYou have entered\n");
+
     for(i=0;i<5;i++){
         printf("%d",arr[i]);
         printf("\n");

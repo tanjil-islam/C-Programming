c9c6ce617bf0886665f7055090863e982b43274f
tanjil-islam <tanjil.islam040820@gmail.com>
2021-05-20 23:48:29 +0600

    Added a programme of mathmetical term

---
+++ mathematics.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a=33,b=77;
+int sum;
+sum=a+b;
+printf("sum =%d\n",sum);
+int subtraction;
+subtraction=a-b;
+printf("subtraction =%d\n",subtraction);
+
+a=33,b=77;
+int multiplication;
+multiplication=a*b;
+printf("product =%d\n",multiplication);
+     a=16,b=4;
+    int division;
+    division=a/b;
+    printf("product=%d",division);
+ a=13,b=2;
+int modulus;
+modulus =a%b;
+printf("\nModulas = %d",modulus);
+
+return 0;
+}

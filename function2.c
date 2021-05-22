ce87d019366a75101000ec3a2fcdfc1ecfbf89c4
tanjil-islam <tanjil.islam040820@gmail.com>
2021-05-22 11:22:05 +0600

    Simple function with argument

---
+++ function2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+void Sum(int a,int b)
+{
+int Sum=a+b;
+printf("Sum=%d\n",Sum);
+}
+
+
+int main()
+{Sum(60,40);
+Sum(600,140);
+Sum(260,401);
+Sum(70,140);
+Sum(360,402);
+
+}
+

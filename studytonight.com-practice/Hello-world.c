b27e5b7acbdd145d5cac3dd44f833dca6f404c0f
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-05 02:28:34 +0600

    Basic Programm

---
+++ studytonight.com-practice/Hello-world.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+
+{
+    printf("\n\n\t\t Study tonight- Best place to learn");
+    printf("\n\nHello world!\nWelcome to study tonight:Best place to learn");
+    printf("\n\n\t\tCoding is fun!");
+
+
+    return 0;
+}

48448471214945e860e810d9f515ebf06108922f
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-05 11:59:53 +0600

    Basic programm

---
+++ studytonight.com-practice/Find-Ascii value of character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudy tonight-Best place to learn");
+
+    char c;
+    printf("\n\nEnter a character:");
+    scanf("%c",&c);
+    printf("\nAscii value is %c=%d",c,c);
+    printf("\n\n\t\t\tCoding is love!");
+
+    return 0;
+}

24d78ceda1089f2ed401e8893165b860001b6df9
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-06 22:39:40 +0600

    Basic programm

---
+++ studytonight.com-practice/Using-ger()function.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n\n\t\tStudy tonight-Best place for learn");
+
+    char name[20];
+
+    printf("\nPlease enter your name:");
+    gets(name);
+    printf("\n\n\tWelcome to study night:%s",name);
+
+    printf("\n\n\t\t\tCoding is fun!");
+    return 0;
+}

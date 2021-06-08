c553aee9f6828b027e449b463bcbe538fbccb334
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-08 23:11:16 +0600

    Loops

---
+++ studytonight.com-practice/Basic-for loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n\n\t\tStudy tonight-Best for learn\n");
+
+     int i=0;
+     for (i=0; i<10; i++)
+     {
+         printf("i=%d\n",i);
+     }
+
+    printf("\n\n\t\t\tCoding is love!");
+    return 0;
+}

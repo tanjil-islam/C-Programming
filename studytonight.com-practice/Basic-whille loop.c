c16eb720c6c00b25435ba73dd75d3fb23eea11ba
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-08 23:45:46 +0600

    Loops

---
+++ studytonight.com-practice/Basic-whille loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    int i=0;
+    while (i<10)
+    {
+        printf("i=%d\n",i);
+        i++;
+    }
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+
+    return 0;
+}

e0ae2f42bc2d3c381286ba08be86b61a8cf3b5c8
tanjil-islam <tanjil.islam040820@gmail.com>
2021-06-20 11:53:04 +0600

    Main theme for Fibonacci series

---
+++ studytonight.com-practice/Fibonacci-series Programm.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i,n,result;
+
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    a=0;
+    b=1;
+    for(i=1; i<=n; i++)
+
+    {
+        printf("%d\n",a);
+        result=a + b;
+        a= b;
+        b= result;
+    }
+    return 0;
+}

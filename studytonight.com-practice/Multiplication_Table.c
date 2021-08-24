4f5ba41df470678e91e7aad59c0764c81aa8a85e
tanjil-islam <tanjil.islam040820@gmail.com>
2021-08-24 12:05:18 +0600

    Program to print the Multiplication Table of any Number.

---
+++ studytonight.com-practice/Multiplication_Table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+
+    printf("Enter your number:");
+    scanf("%d",&n);
+
+
+    for(i=1; i<=10; i++)
+    {
+        printf("\n%d*%d=%d",n,i,n*i);
+    }
+
+
+    return 0;
+}
